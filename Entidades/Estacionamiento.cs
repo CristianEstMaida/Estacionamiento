89b7da3a42fae9f3208351689e4e665a04ba2f58
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2021-12-10 13:19:12 -0300

    Add files via upload

---
+++ Entidades/Estacionamiento.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Estacionamiento
+    {
+        private int capacidadEstacionamiento;
+        private string nombre;
+        private List<Vehiculo> listadoVehiculos;
+        private static Estacionamiento estacionamiento;
+
+        private Estacionamiento(string nombre, int capacidad)
+        {
+            this.listadoVehiculos = new List<Vehiculo>();
+            this.nombre = nombre;
+            this.capacidadEstacionamiento = capacidad;
+        }
+        public List<Vehiculo> ListadoVehiculos
+        {
+            get { return this.listadoVehiculos; }
+        }
+        public string Nombre
+        {
+            get { return this.nombre; }
+        }
+
+        public static Estacionamiento GetEstacionamiento(string nombre, int capacidad)
+        {
+            if (Estacionamiento.estacionamiento is null)
+            {
+                Estacionamiento.estacionamiento = new Estacionamiento(nombre, capacidad);
+            }
+            else
+            {
+                Estacionamiento.estacionamiento.capacidadEstacionamiento = capacidad;
+            }
+            return Estacionamiento.estacionamiento;
+        }
+        public string InformarSalida(Vehiculo vehiculo)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Retiro Estacionamiento {this.nombre}");
+            sb.AppendLine(vehiculo.ToString());
+            sb.AppendLine($"Hora de salida: {vehiculo.HoraEgreso}");
+            sb.AppendLine($"El cargo por estacionamiento es: {vehiculo.CostoEstadia.ToString("00.0")}");
+            return sb.ToString();
+        }
+
+
+        public static bool operator +(Estacionamiento estacionamiento, Vehiculo vehiculo)
+        {
+            bool returnAux = false;
+            if (estacionamiento.listadoVehiculos.Count < estacionamiento.capacidadEstacionamiento && estacionamiento != vehiculo)
+            {
+                Estacionamiento.estacionamiento.listadoVehiculos.Add(vehiculo);
+                returnAux = true;
+            }
+            return returnAux;
+        }
+        public static bool operator -(Estacionamiento estacionamiento, Vehiculo vehiculo)
+        {
+            bool returnAux = false;
+            if (estacionamiento == vehiculo)
+            {
+                vehiculo.HoraEgreso = DateTime.Now;
+                Estacionamiento.estacionamiento.listadoVehiculos.Remove(vehiculo);
+                return true;
+            }
+            return returnAux;
+        }
+        public static bool operator ==(Estacionamiento estacionamiento, Vehiculo vehiculo)
+        {
+            bool returnAux = false;
+            if (Estacionamiento.estacionamiento.listadoVehiculos.Count > 0)
+            {
+                foreach (Vehiculo v in estacionamiento.listadoVehiculos)
+                {
+                    if (v == vehiculo)
+                    {
+                        returnAux = true;
+                        break;
+                    }
+                }
+            }
+            return returnAux;
+        }
+        public static bool operator !=(Estacionamiento estacionamiento, Vehiculo vehiculo)
+        {
+            return !(estacionamiento == vehiculo);
+        }
+    }
+
+}
---
+++ Entidades/Vehiculo.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public abstract class Vehiculo
+    {
+        
+        public enum EVehiculos { Automovil, Moto };
+        private DateTime horaIngreso;
+        private DateTime horaEgreso;
+        private string patente;
+        public Vehiculo(string patente, DateTime horaIngreso)
+        {
+            this.horaIngreso = horaIngreso;
+            this.patente = patente;
+        }
+        public string Patente
+        {
+            get 
+            {
+                return this.patente;
+            }
+            set 
+            {
+                if(this.ValidarPatente(value))
+                {
+                    this.patente = value;
+                }
+            }
+        }
+        public abstract string Descripcion{ get; }
+        public abstract double CostoEstadia { get; }
+        public DateTime HoraIngreso { get { return this.horaIngreso; }}
+
+        public DateTime HoraEgreso
+        {
+            get
+            {
+                return this.horaEgreso;
+            }
+            set
+            {
+                if (value > this.horaIngreso)
+                {
+                    this.horaEgreso = value;
+                }
+            }
+        }
+
+        private bool ValidarPatente(string patente)
+        {
+            bool returnAux = false;
+            if (!string.IsNullOrEmpty(patente) && patente.Length >= 6 && patente.Length < 8)
+            {
+                returnAux = true;
+            }
+            return returnAux;
+        }
+        
+        protected virtual double CargoDeEstacionamiento()
+        {
+            double horasAcumuladas= 0;
+            if (this.HoraEgreso > this.HoraIngreso)
+            {
+                horasAcumuladas = (this.HoraEgreso - this.horaIngreso).TotalHours;
+            }
+            return horasAcumuladas;
+        }
+        protected virtual string MostrarDatos()
+        {
+            StringBuilder returnAux = new StringBuilder();
+            returnAux.AppendLine($"Patente {this.Patente}");
+            returnAux.AppendLine($"Ingreso {this.HoraIngreso}");
+            return returnAux.ToString();
+        }
+
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.Patente == v2.Patente);
+        }
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 == v2);
+        }
+    }
+}
